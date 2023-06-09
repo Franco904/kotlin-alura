b32846a5aab25ec6a02db640d3e2dfd61b25b97a
francostavares <francostavares2003@gmail.com>
2023-06-09 17:50:32 -0300

    update exception class parameter

--- bytebank/src/main/kotlin/br/com/alura/bytebank/exceptions/SaldoInsuficienteException.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/exceptions/SaldoInsuficienteException.kt
@@ -1,7 +1,7 @@
 package br.com.alura.bytebank.exceptions
 
 class SaldoInsuficienteException : Exception(
-    message = "O saldo desta conta é insuficiente para realizar a transferência solicitada.",
+    "O saldo desta conta é insuficiente para realizar a transferência solicitada.",
 ) {
     override fun printStackTrace() {
         println("[SaldoInsuficienteException] Stacktrace:")
